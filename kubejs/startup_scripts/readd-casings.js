b28aa3c3dcc8ac1fa6131a8c0b52e419bf664973
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2026-03-21 14:13:58 -0400

    Script to re-enable radiant and shadow casings in Create

---
+++ kubejs/startup_scripts/readd-casings.js
@@ -0,0 +1,7 @@
+StartupEvents.modifyCreativeTab('create:base', event => {
+    event.addAfter('create:brass_ingot', 'create:chromatic_compound')
+    event.addAfter('create:brass_ingot', 'create:refined_radiance')
+    event.addAfter('create:brass_ingot', 'create:shadow_steel')
+    event.addAfter('create:copper_casing', 'create:refined_radiance_casing')
+    event.addAfter('create:copper_casing', 'create:shadow_steel_casing')
+})
