186adcdc11f79ee88feb24876529009ce237e48b
Arikelliott <20728146+Arikelliott@users.noreply.github.com>
2026-07-11 22:40:21 -0400

    Added copper tool recipes

--- kubejs/server_scripts/tools.js
+++ kubejs/server_scripts/tools.js
@@ -1,8 +1,58 @@
 ServerEvents.recipes(event => {
+
+    // Add copper tools:
+    event.shaped(
+        Item.of('aethernauts:copper_sword', 1),
+        [
+            ' A ',
+            ' A ',
+            ' B '
+        ],
+        {
+            A: 'minecraft:copper_ingot',
+            B: 'minecraft:stick'
+        }
+    )
     event.shaped(
         Item.of('aethernauts:copper_pickaxe', 1),
         [
             'AAA',
+            ' B ',
+            ' B '
+        ],
+        {
+            A: 'minecraft:copper_ingot',
+            B: 'minecraft:stick'
+        }
+    )
+    event.shaped(
+        Item.of('aethernauts:copper_axe', 1),
+        [
+            'AA ',
+            'AB ',
+            ' B '
+        ],
+        {
+            A: 'minecraft:copper_ingot',
+            B: 'minecraft:stick'
+        }
+    )
+    event.shaped(
+        Item.of('aethernauts:copper_shovel', 1),
+        [
+            ' A ',
+            ' B ',
+            ' B '
+        ],
+        {
+            A: 'minecraft:copper_ingot',
+            B: 'minecraft:stick'
+        }
+    )
+    event.shaped(
+        Item.of('aethernauts:copper_hoe', 1),
+        [
+            'AA ',
             ' B ',
             ' B '
         ],
